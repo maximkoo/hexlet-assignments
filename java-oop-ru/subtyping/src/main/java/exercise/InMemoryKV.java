9da8ec1e73c669bbf69bb7145e79fc0e32ae3ac3
maximkoo <svazarm@yandex.ru>
2024-07-31 21:31:05 +0300

    Downloaded subtypes

---
+++ java-oop-ru/subtyping/src/main/java/exercise/InMemoryKV.java
@@ -0,0 +1,14 @@
+package exercise;
+
+import java.util.Map;
+import java.util.HashMap;
+import KeyValueStorage;
+
+// BEGIN
+public class InMemoryKV implements KeyValueStorage{
+    public static void main(String[] args) {
+
+    }
+
+}
+// END
