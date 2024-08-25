9d060cc5acb069f16cb376f6839bbc1f177dc054
maximkoo <svazarm@yandex.ru>
2024-08-25 20:15:43 +0300

    Subtyping: InMemoryKV passed 4 tests

---
+++ java-oop-ru/subtyping/src/main/java/exercise/App.java
@@ -0,0 +1,36 @@
+package exercise;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+// BEGIN
+public class App{
+    public static void main(String[] args) {
+        KeyValueStorage storage = new InMemoryKV(Map.of("key1", "value1"));
+        storage.set("key2", "value2");
+        System.out.println("ALL DONE!!!");
+        swapKeyValue(storage);
+        System.out.println("My Test");
+        System.out.println(storage.get("value2",""));
+    }
+
+    public static void swapKeyValue(KeyValueStorage kv){
+
+        Map<String,String> map2 = new HashMap<>();
+        for (Entry<String, String> s : kv.toMap().entrySet()) {
+            map2.put(s.getKey(), s.getValue());
+        }
+
+        for (String s : map2.keySet()) {
+            kv.unset(s);
+        }
+
+        for (Entry<String, String> s : map2.entrySet()) {
+            kv.set(s.getValue(),s.getKey());
+        }
+
+    }
+}
+// END
