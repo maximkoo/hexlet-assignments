51d6fdc5571c27c3cb6777aab7ca1fa94c968906
@hexlet/cli <support@hexlet.io>
2025-04-21 19:50:54 +0300

    submit java-web-ru/session

--- java-web-ru/session/src/main/java/exercise/App.java
+++ java-web-ru/session/src/main/java/exercise/App.java
@@ -17,7 +17,7 @@ public final class App {
 
         // BEGIN
         app.get(NamedRoutes.buildSessionPath(), SessionsController::build);
-        //app.get(NamedRoutes.rootPath(), SessionsController::build);
+        app.get(NamedRoutes.rootPath(), SessionsController::build);
         app.post(NamedRoutes.loginPath(), SessionsController::create);
         app.post(NamedRoutes.logoutPath(), SessionsController::destroy);
         // END
